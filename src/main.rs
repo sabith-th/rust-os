5cf9833591a74404a0a6650d11add8485b41fb2f
Sabith T H <sabith.th.07@gmail.com>
2020-05-27 21:05:28 +0530

    create a global writer and a println macro

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,23 @@
 #![no_main]
 
 mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop{}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again!").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {} \n", 42, 1.337).unwrap();
 
+    println!("Hello There {}", "General Kenobi");
+
+    panic!("Something's wrong I can feel it");
     loop {}
 }
