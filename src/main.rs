761a2ff4f143876e4136d5111b771ad93d2dca74
Sabith T H <sabith.th.07@gmail.com>
2020-05-28 20:57:02 +0530

    create a custom test framework, use serial port to write to host console

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
+mod serial;
 
 use core::panic::PanicInfo;
+use crate::QemuExitCode::Success;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop{}
+    loop {}
 }
 
 #[no_mangle]
@@ -17,8 +22,43 @@ pub extern "C" fn _start() -> ! {
     vga_buffer::WRITER.lock().write_str("Hello again!").unwrap();
     write!(vga_buffer::WRITER.lock(), ", some numbers: {} {} \n", 42, 1.337).unwrap();
 
-    println!("Hello There {}", "General Kenobi");
+    println!("Avengers {}", "Assemble");
 
-    panic!("Something's wrong I can feel it");
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("Trivial assertion...");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
