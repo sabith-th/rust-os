5c9a4fca3d0157db7c7754482b1e68499302dd65
Sabith T H <sabith.th.07@gmail.com>
2020-05-29 19:33:12 +0530

    create simple integration tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,27 +5,23 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use rust_os::println;
+use rust_os::{println, serial_print, serial_println};
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
     loop {}
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rust_os::test_panic_handler(info)
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Avengers {}", "Assemble");
-
-    #[cfg(test)]
-        test_main();
-
-    loop {}
+#[test_case]
+fn test_println() {
+    serial_print!("test_println... ");
+    println!("test_println output");
+    serial_println!("[ok]");
 }
